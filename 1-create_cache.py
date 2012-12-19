3296ca1258fe7f43c8a58c537450366d3a533f7a
Shantanu Singh <shsingh@broadinstitute.org>
2012-12-19 13:34:57 -0500

    Split into classes using FCP and use image features to predict class

--- 1-create_cache.py
+++ 1-create_cache.py
@@ -1,17 +1,33 @@
+#!/usr/bin/env python
 import os
+import sys
+from optparse import OptionParser
 import fnmatch
 
-ddir="../input/"
-cache_tag="2012-11-28"
-datasets=["TIALP-B1"]
-for dataset in datasets:
-    for i, fname in enumerate(os.listdir(os.path.join(ddir, dataset))):
-        if fnmatch.fnmatch(fname, '*.properties'):
-            prop_file = os.path.join(ddir, dataset, fname)
-            cache_dir = os.path.join(ddir, dataset, "cache-" + cache_tag)
-            cmd = "python -m cpa.profiling.cache -r %s %s %s" % \
-                  (prop_file, cache_dir, '""')
-            bsub="bsub -q week -J %s -o %s.out " % \
-                  ("cache-" + dataset, "cache-" + dataset)
 
-            print bsub + cmd
+show_progress = True
+parser = OptionParser("usage: %prog <dataset-name> ")
+options, args = parser.parse_args()
+
+if len(args) != 1:
+    parser.error('Incorrect number of arguments')
+
+dataset_name = sys.argv[1]
+
+ddir="../input/"
+flag=False
+for i, fname in enumerate(os.listdir(os.path.join(ddir, dataset_name))):
+    if fnmatch.fnmatch(fname, '*.properties'):
+        prop_file = os.path.join(ddir, dataset_name, fname)
+        cache_dir = os.path.join(ddir, dataset_name, "cache")
+        cmd = "python -m cpa.profiling.cache -r %s %s %s" % \
+              (prop_file, cache_dir, '""')
+        bsub="bsub -q week -J %s -o %s.out " % \
+              ("cache-" + dataset_name, "cache-" + dataset_name)
+
+        print bsub + cmd
+        flag=True
+
+
+if not flag:
+    parser.error('Dataset %s does not exist' % dataset_name)
--- 3-create_profiles.sh
+++ 3-create_profiles.sh
@@ -3,16 +3,11 @@
 lsfdir=.compute_`date +%s%N` \
 dataset_name=$1
 
-if [ $dataset_name = "TIALP-B1" ]
-then
-    properties_file=2012_08_20_TopoChipScreening_deBoerLab_TiALP.properties
-    cache_dir=cache-2012-11-28
-else
-    echo Dataset $dataset_name does not exist.
-    exit
-fi;
-
 datadir=../input/${dataset_name}/
+cache_dir=cache
+properties_file=`find $datadir -name "*.properties"`
+properties_file=`basename $properties_file`
+
 curdir=`pwd`
 OS=`uname`
 if [ $OS == "Darwin" ]
@@ -26,32 +21,34 @@ else
     parallel_option=lsf-directory=$lsfdir
 #    parallel_option=multiprocessing
 fi
-cd $datadir
-
 
 # ---------- Create profiles  ----------
 
 function run_profile_gen {
+  echo 
+  echo 
+  set -x
+  echo \#Computing profiles ${normalization_tag}:${normtype}:${method}
+  csvname=well-summary-${profile_method}-${method}-${normalization_tag}-${normtype}.csv
   rm -rf $lsfdir
-  echo $PYTHONBIN -m cpa.profiling.profile_mean -c -o $csvname  --${parallel_option} --normalization=$normalization --method=$method $properties_file $cache_dir Design
-  echo $SED -i 's/Design,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,/Image_Metadata_ArrayNumber,Image_Metadata_CA,Image_Metadata_CCD,Image_Metadata_CCDunsc,Image_Metadata_ChipCOL,Image_Metadata_ChipROW,Image_Metadata_CircArea,Image_Metadata_CircDiam,Image_Metadata_DC,Image_Metadata_DL,Image_Metadata_DT,Image_Metadata_FCP,Image_Metadata_FCPLOG,Image_Metadata_FCPLOGN0_1,Image_Metadata_FCPLOGN0_3,Image_Metadata_FCPN01,Image_Metadata_FCPN03,Image_Metadata_FeatSize,Image_Metadata_FileName_ALP_w_array_name,Image_Metadata_FileName_Actin_w_array_name,Image_Metadata_FileName_DNA_w_array_name,Image_Metadata_LA,Image_Metadata_LineArea,Image_Metadata_LineLen,Image_Metadata_NUM,Image_Metadata_NumCirc,Image_Metadata_NumLine,Image_Metadata_NumTri,Image_Metadata_ROT,Image_Metadata_RotSD,Image_Metadata_Split,Image_Metadata_TA,Image_Metadata_TriArea,Image_Metadata_TriSize,Image_Metadata_WN0_1,Image_Metadata_WN0_2,Image_Metadata_WN0_3,Image_Metadata_WN0_4,Image_Metadata_WN0_5,Image_Metadata_WN0_7,Image_Metadata_WN1,Image_Metadata_WN1_5,Image_Metadata_WN2,Image_Metadata_WN3,Image_Metadata_WN4,Image_Metadata_colidxbottom,Image_Metadata_colidxtop,Image_Metadata_featureidx,Image_Metadata_imageidxbottom,Image_Metadata_imageidxtop,Image_Metadata_internalidx,Image_Metadata_rowidxbottom,Image_Metadata_rowidxtop,Image_Metadata_unitidxbottom,Image_Metadata_unitidxtop,/1' $csvname
+  cd $datadir
+  $PYTHONBIN -m cpa.profiling.${profile_method} -c -g -o $csvname  --${parallel_option} --normalization=$normalization --method=${method} $properties_file $cache_dir $group
+  rm -rf $lsfdir
+  cd -
+  echo 
 }
 
-####
-echo Computing profiles without normalization - median+mad
-profile_method=profile_mean
-normtype=no_norm
-method=median+mad
-csvname=well-${method}-${normtype}.csv
-normalization=DummyNormalization
-run_profile_gen
+# function run_profile_gen {
+#   rm -rf $lsfdir
+#   echo $PYTHONBIN -m cpa.profiling.profile_mean -c -o $csvname  --${parallel_option} --normalization=$normalization --method=$method $properties_file $cache_dir Design
+#   echo $SED -i 's/Design,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,/Image_Metadata_ArrayNumber,Image_Metadata_CA,Image_Metadata_CCD,Image_Metadata_CCDunsc,Image_Metadata_ChipCOL,Image_Metadata_ChipROW,Image_Metadata_CircArea,Image_Metadata_CircDiam,Image_Metadata_DC,Image_Metadata_DL,Image_Metadata_DT,Image_Metadata_FCP,Image_Metadata_FCPLOG,Image_Metadata_FCPLOGN0_1,Image_Metadata_FCPLOGN0_3,Image_Metadata_FCPN01,Image_Metadata_FCPN03,Image_Metadata_FeatSize,Image_Metadata_FileName_ALP_w_array_name,Image_Metadata_FileName_Actin_w_array_name,Image_Metadata_FileName_DNA_w_array_name,Image_Metadata_LA,Image_Metadata_LineArea,Image_Metadata_LineLen,Image_Metadata_NUM,Image_Metadata_NumCirc,Image_Metadata_NumLine,Image_Metadata_NumTri,Image_Metadata_ROT,Image_Metadata_RotSD,Image_Metadata_Split,Image_Metadata_TA,Image_Metadata_TriArea,Image_Metadata_TriSize,Image_Metadata_WN0_1,Image_Metadata_WN0_2,Image_Metadata_WN0_3,Image_Metadata_WN0_4,Image_Metadata_WN0_5,Image_Metadata_WN0_7,Image_Metadata_WN1,Image_Metadata_WN1_5,Image_Metadata_WN2,Image_Metadata_WN3,Image_Metadata_WN4,Image_Metadata_colidxbottom,Image_Metadata_colidxtop,Image_Metadata_featureidx,Image_Metadata_imageidxbottom,Image_Metadata_imageidxtop,Image_Metadata_internalidx,Image_Metadata_rowidxbottom,Image_Metadata_rowidxtop,Image_Metadata_unitidxbottom,Image_Metadata_unitidxtop,/1' $csvname
+# }
 
 ####
-echo Computing profiles without normalization - mean
 profile_method=profile_mean
-normtype=no_norm
-method=mean
-csvname=well-${method}-${normtype}.csv
 normalization=DummyNormalization
+normalization_tag=none
+normtype=none
+method=median
+group=Design
 run_profile_gen
-
